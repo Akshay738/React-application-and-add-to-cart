1c899cdfa96675cc98f0d87ab5f6ba7495d46ef2
Akshay738 <akshay.99tech@gmail.com>
2023-04-01 09:41:51 +0530

    React-application-and-add-to-car

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Product from './components/Product';
+import './App.css';
+import List from './components/List';
+import Application from './components/Application';
+
+function App() {
+  return (
+    <div className="App">
+      <Product /><hr />
+      <List /><hr />
+      <Application />
+     
+    </div>
+  );
+}
+
+export default App;
